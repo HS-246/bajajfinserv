2bbb3bf637e4bc649a7096e3fa07331769e7df9e
HS-246 <happyhusain12@gmail.com>
2025-12-01 17:02:16 +0530

    tried solving webhook not being got

--- src/main/java/com/husain/bajajfinserv/BajajFinservApplication.java
+++ src/main/java/com/husain/bajajfinserv/BajajFinservApplication.java
@@ -2,6 +2,8 @@ package com.husain.bajajfinserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BajajFinservApplication {
@@ -9,6 +11,11 @@ public class BajajFinservApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(BajajFinservApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- src/main/java/com/husain/bajajfinserv/dto/WebhookResponse.java
+++ src/main/java/com/husain/bajajfinserv/dto/WebhookResponse.java
@@ -1,13 +1,19 @@
 package com.husain.bajajfinserv.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor      // ← Jackson needs this to create empty object
+@AllArgsConstructor     // ← Still needed for creating objects manually
+@Getter
 public class WebhookResponse {
+    @JsonProperty("webhook")        // ← Maps API response field
     private String webhook;
+
+    @JsonProperty("accessToken")    // ← Maps API response field
     private String accessToken;
 }
