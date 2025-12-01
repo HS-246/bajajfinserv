1b932e38b67083352fd88d68bbd30d37c783162f
HS-246 <happyhusain12@gmail.com>
2025-12-01 17:15:28 +0530

    fixed errors and add jar

--- src/main/java/com/husain/bajajfinserv/dto/SubmissionRequest.java
+++ src/main/java/com/husain/bajajfinserv/dto/SubmissionRequest.java
@@ -3,8 +3,22 @@ package com.husain.bajajfinserv.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
-@AllArgsConstructor
 public class SubmissionRequest {
+
     private String finalQuery;
+
+    public SubmissionRequest() {
+    }
+
+    public SubmissionRequest(String finalQuery) {
+        this.finalQuery = finalQuery;
+    }
+
+    public String getFinalQuery() {
+        return finalQuery;
+    }
+
+    public void setFinalQuery(String finalQuery) {
+        this.finalQuery = finalQuery;
+    }
 }
--- src/main/java/com/husain/bajajfinserv/dto/WebhookResponse.java
+++ src/main/java/com/husain/bajajfinserv/dto/WebhookResponse.java
@@ -1,19 +1,33 @@
 package com.husain.bajajfinserv.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor      // ← Jackson needs this to create empty object
-@AllArgsConstructor     // ← Still needed for creating objects manually
-@Getter
+
 public class WebhookResponse {
-    @JsonProperty("webhook")        // ← Maps API response field
-    private String webhook;
 
-    @JsonProperty("accessToken")    // ← Maps API response field
+    private String webhook;
     private String accessToken;
+
+    public WebhookResponse() {
+    }
+
+    public WebhookResponse(String webhook, String accessToken) {
+        this.webhook = webhook;
+        this.accessToken = accessToken;
+    }
+
+    public String getWebhook() {
+        return webhook;
+    }
+
+    public void setWebhook(String webhook) {
+        this.webhook = webhook;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 }
